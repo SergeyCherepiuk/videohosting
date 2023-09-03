4ee2b1e7103c994750c2e9b427073bd0c46cfc1a
Sergey Cherepiuk <s.cherepiuk@gmail.com>
2023-09-03 08:49:14 +0200

    Styled Label component with tailwind

--- client/src/components/upload/Label.svelte
+++ client/src/components/upload/Label.svelte
@@ -2,27 +2,7 @@
     export let label: string
 </script>
 
-<div class="column">
-    <span>{label}</span>
+<div class="flex flex-col gap-2">
+    <span class="font-amazon-ember text-lg ps-4">{label}</span>
     <slot />
 </div>
-
-<style lang="scss">
-    .column {
-        display: flex;
-        flex-direction: column;
-        gap: 6px;
-    }
-
-    span {
-        font-family: "Amazon Ember";
-        font-size: 18px;
-        padding-left: 16px;
-    }
-
-    @font-face {
-        font-family: "Amazon Ember";
-        src: local("../../../static")
-            url("amazon_ember.ttf") format("truetype")
-    }
-</style>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,6 +2,9 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
+    fontFamily: {
+      "amazon-ember": ["Amazon Ember"]
+    },
     extend: {},
   },
   plugins: [],
