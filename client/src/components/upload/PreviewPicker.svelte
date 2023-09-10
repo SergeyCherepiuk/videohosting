ed6f9a3aaea054d963510620427dd8da32571c80
Sergey Cherepiuk <s.cherepiuk@gmail.com>
2023-09-10 15:08:59 +0200

    Not finished but at least working PreviewPicker

--- client/src/components/upload/PreviewPicker.svelte
+++ client/src/components/upload/PreviewPicker.svelte
@@ -1,39 +1,47 @@
 <script lang="ts">
-    import { onMount } from "svelte";
+    import Button from "../common/Button.svelte";
 
-    export let preview: Blob
+    export let preview: Blob | null = null
     export let video: Blob
-    export let alt: string
-
-    let videoSrc = URL.createObjectURL(video)
-    let previewSrc: string | null = null
-    
-    onMount(() => {
-        let canvas = document.getElementById("canvas") as HTMLCanvasElement
-        let player = document.getElementById("player") as HTMLVideoElement
-        canvas.getContext("2d")?.drawImage(player, 0, 0, player.videoWidth, player.videoHeight)
-        canvas.toBlob(blob => { if (blob) preview = blob }) // TODO: Returns empty blob
-    })
+    export let alt: string 
 
     let previewFile: FileList
     function onSelect() {
         preview = previewFile.item(0) as Blob
-        setPreviewSrc(preview)
     }
 
-    function setPreviewSrc(image: Blob) {
-        previewSrc = URL.createObjectURL(image)
+    function generatePreview() {
+        let videoElement = document.getElementById("player") as HTMLVideoElement
+        let canvasElement = document.getElementById("canvas") as HTMLCanvasElement
+        let ctx = canvasElement.getContext("2d")
+        ctx?.drawImage(videoElement, 0, 0, canvasElement.width, canvasElement.height)
+        canvasElement.toBlob(blob => { if (blob) preview = blob })
+    }
+
+    function clearPreview() {
+        preview = null
     }
 </script>
 
-<canvas id="canvas" class="hidden" />
-<label for="preview" class="aspect-video rounded-xl overflow-clip bg-black">
-    {#if previewSrc}
-        <img id="image" src={previewSrc} {alt}
-            class="w-full aspect-video rounded-xl bg-black object-contain" />
+{#if preview}
+    <button on:click={clearPreview}><span class="text-4xl">&times;</span></button>
+{/if}
+
+<label for="preview" class="flex aspect-video rounded-xl overflow-clip bg-black justify-center items-center">
+    {#if preview}
+        <img id="image" src={URL.createObjectURL(preview)} {alt}
+            class="w-full aspect-video rounded-xl bg-black object-contain" /> 
     {:else}
-        <video id="player" src={videoSrc} />
-    {/if}
+        <div class="flex flex-col items-center gap-4">
+            <Button text="Generate preview" isPrimary={false} onClick={generatePreview} />
+            <p class="text-white text-lg">Or click outside to <u>choose</u></p>
+        </div>
+    {/if} 
+
+    <video id="player" src={URL.createObjectURL(video)} class="w-0">
+        <track kind="captions" />
+    </video>
+    <canvas id="canvas" class="hidden" />
 </label>
 <input id="preview" type="file" class="hidden" 
     bind:files={previewFile}  on:change={onSelect} />
