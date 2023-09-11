8734d246437eeee5898420ceac09d1801b40afbd
Sergey Cherepiuk <s.cherepiuk@gmail.com>
2023-09-11 19:48:49 +0200

    Deleted whole client-side code (planning rewrite in HTMX)

--- client/src/components/common/Label.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-    export let label: string
-</script>
-
-<div class="flex flex-col gap-2">
-    <span class="font-amazon-ember text-lg ps-4">{label}</span>
-    <slot />
-</div>
--- client/src/components/upload/ProgressBar.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-    export let progress: number
-</script>
-
-<div class="flex flex-row w-full h-1.5 rounded-full overflow-hidden bg-gray-70">
-    <div class="bg-blue-50" style="width: {progress}%"></div>
-</div>
